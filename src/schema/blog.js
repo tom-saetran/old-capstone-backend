2a7484f82ccfb5aab141575313305df4dc5e1b27
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-06-12 01:16:20 +0200

    Update blog.js

--- src/schema/blog.js
+++ src/schema/blog.js
@@ -13,7 +13,7 @@ const blogSchema = new Schema(
     {
         category: { type: String, required: true },
         title: { type: String, required: true },
-        cover: { type: String, default: "https://via.placeholder.com/420x200?text=Cover%20Banner" },
+        cover: { type: String, default: null },
         author: { type: Schema.Types.ObjectId, ref: "User", required: true },
         content: { type: String, required: true },
         comments: { type: [commentSchema], default: [] },
